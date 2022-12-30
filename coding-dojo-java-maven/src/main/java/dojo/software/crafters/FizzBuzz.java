1a791d44c1661f3153f4e3ba21302bbe3ae7f70e
Paolo Sciarra <itbhps@gmail.com>
2022-12-30 22:07:10 +0100

    refactored code: using functional concept of Monoid

--- coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
+++ coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
@@ -7,13 +7,34 @@ public class FizzBuzz {
     /*
         Code smells:
 
-        1. Duplications: mapping value Fizz and Buzz are repeated
-        2. Somehow also the divisible logic is repeated
-        3. Temporal coupling: if we change the order of the ifs the code will not work
+        1. Duplications: mapping value Fizz and Buzz are repeated (-> solved)
+        2. Somehow also the divisible logic is repeated  (-> solved)
+        3. Temporal coupling: if we change the order of the ifs the code will not work  (-> solved)
      */
 
-    record Rule(String motto, int divisor) {
-        String applyTo(int number) {
+    static GameRule combineGameRules(GameRule first, GameRule second) {
+        return number -> {
+            var firstResult = first.applyTo(number);
+            var secondResult = second.applyTo(number);
+            return firstResult + secondResult;
+        };
+    }
+
+    interface GameRule {
+        String applyTo(int number);
+    }
+
+    static class NumberRule implements GameRule {
+        private final String motto;
+        private final int divisor;
+
+        NumberRule(String motto, int divisor) {
+            this.motto = motto;
+            this.divisor = divisor;
+        }
+
+        @Override
+        public String applyTo(int number) {
             if (isSatisfiedBy(number)) {
                 return motto;
             } else {
@@ -26,15 +47,23 @@ public class FizzBuzz {
         }
     }
 
-    public static String fizzBuzz(int number) {
-        // refactoring removing mutability
-        var rules = List.of(new Rule("Fizz", 3), new Rule("Buzz", 5));
+    enum IdentityRule implements GameRule {
+        INSTANCE;
 
-        String result = rules.stream()
-                .map(rule -> rule.applyTo(number))
-                .reduce("", String::concat);
-
-        return result.isEmpty() ? String.valueOf(number) : result;
+        @Override
+        public String applyTo(int number) {
+            return "";
+        }
     }
 
+    public static String fizzBuzz(int number) {
+        List<GameRule> rules = List.of(new NumberRule("Fizz", 3), new NumberRule("Buzz", 5));
+
+        GameRule identityRule = IdentityRule.INSTANCE;
+        GameRule result = rules.stream()
+                .reduce(identityRule, FizzBuzz::combineGameRules);
+
+        String rulesResult = result.applyTo(number);
+        return rulesResult.isEmpty() ? String.valueOf(number) : rulesResult ;
+    }
 }
