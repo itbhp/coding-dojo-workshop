c1e11fe21900173add6e6cb0a9efd4f092e04dfd
Paolo Sciarra <itbhps@gmail.com>
2022-12-30 20:42:15 +0100

    refactored code using mutability

--- coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
+++ coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
@@ -1,5 +1,9 @@
 package dojo.software.crafters;
 
+import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.Predicate;
+
 public class FizzBuzz {
 
     /*
@@ -10,16 +14,23 @@ public class FizzBuzz {
         3. Temporal coupling: if we change the order of the ifs the code will not work
      */
 
+    record Rule(String motto, Predicate<Integer> predicate) {
+        boolean isSatisfiedBy(int number) {
+            return predicate.test(number);
+        }
+    }
+
     public static String fizzBuzz(int number) {
-        if (number % 3 == 0 && number % 5 == 0) {
-            return "FizzBuzz";
-        }
-        if (number % 5 == 0) {
-            return "Buzz";
-        }
-        if (number % 3 == 0) {
-            return "Fizz";
-        }
-        return String.valueOf(number);
+        // refactoring using mutability
+        AtomicReference<String> result = new AtomicReference<>("");
+        Predicate<Integer> fizzTest = n -> n % 3 == 0;
+        Predicate<Integer> buzzTest = n -> n % 5 == 0;
+        var rules = List.of(new Rule("Fizz", fizzTest), new Rule("Buzz", buzzTest));
+        rules.forEach(rule ->
+                result.updateAndGet(previous ->
+                        rule.isSatisfiedBy(number) ? previous + rule.motto : previous
+                )
+        );
+        return result.get().isEmpty()? String.valueOf(number): result.get();
     }
 }
