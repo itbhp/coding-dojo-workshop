29d9fea9a8911bc1865190603f0dd52671a20f58
Paolo Sciarra <itbhps@gmail.com>
2022-12-30 19:34:27 +0100

    now we have listed the code smells

--- coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
+++ coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
@@ -1,8 +1,17 @@
 package dojo.software.crafters;
 
 public class FizzBuzz {
+
+    /*
+        Code smells:
+
+        1. Duplications: mapping value Fizz and Buzz are repeated
+        2. Somehow also the divisible logic is repeated
+        3. Temporal coupling: if we change the order of the ifs the code will not work
+     */
+
     public static String fizzBuzz(int number) {
-        if (number % 15 == 0) {
+        if (number % 3 == 0 && number % 5 == 0) {
             return "FizzBuzz";
         }
         if (number % 5 == 0) {
