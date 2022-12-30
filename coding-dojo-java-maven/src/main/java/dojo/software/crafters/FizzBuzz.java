de1ad01a70c32eabe5cc3d8e1366dc5a9ff51dc7
Paolo Sciarra <itbhps@gmail.com>
2022-12-30 19:10:19 +0100

    now we all non fizz and non buzz numbers working

--- coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
+++ coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
@@ -2,12 +2,6 @@ package dojo.software.crafters;
 
 public class FizzBuzz {
     public static String fizzBuzz(int number) {
-        if (number == 4) {
-            return "4";
-        }
-        if (number == 2) {
-            return "2";
-        }
-        return "1";
+        return String.valueOf(number);
     }
 }
