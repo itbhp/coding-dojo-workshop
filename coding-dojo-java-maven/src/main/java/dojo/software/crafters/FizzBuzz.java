fbc1ca843a6245f0f534c0b18bcc02934b6bb21b
Paolo Sciarra <itbhps@gmail.com>
2023-02-09 23:50:24 +0100

    one commit solution

--- coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
+++ coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
@@ -1,8 +1,47 @@
 package dojo.software.crafters;
 
-public class FizzBuzz 
-{
+import java.util.List;
+
+public class FizzBuzz {
+
+    interface Rule {
+        String adapt(int number);
+
+        static Rule combine(Rule first, Rule second){
+            return number -> {
+                var r1 = first.adapt(number);
+                var r2 = second.adapt(number);
+                return r1 + r2;
+            };
+        }
+
+        Rule identity = number -> "";
+    }
+
+    static class NumberRule implements Rule {
+        private final String motto;
+        private final int divisor;
+
+        public NumberRule(String motto, int divisor) {
+            this.motto = motto;
+            this.divisor = divisor;
+        }
+
+        @Override
+        public String adapt(int number) {
+            if (number % divisor == 0) {
+                return motto;
+            }
+            return "";
+        }
+    }
+
     public static String fizzBuzz(int number) {
-        throw new UnsupportedOperationException();
+        var rules = List.of(new NumberRule("Fizz", 3), new NumberRule("Buzz", 5));
+        var combinedRule = rules.stream().reduce(Rule.identity, Rule::combine, (rule, rule2) -> {
+            throw new UnsupportedOperationException();
+        });
+        var result = combinedRule.adapt(number);
+        return result.equals("") ? String.valueOf(number) : result;
     }
 }
--- coding-dojo-java-maven/src/test/java/dojo/software/crafters/FizzBuzzTest.java
+++ coding-dojo-java-maven/src/test/java/dojo/software/crafters/FizzBuzzTest.java
@@ -1,14 +1,50 @@
 package dojo.software.crafters;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static dojo.software.crafters.FizzBuzz.fizzBuzz;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    @Test
-    void one_should_print_one() {
-        assertThat(fizzBuzz(1)).isEqualTo("1");
+    @ParameterizedTest(name = "{0} should be {1}")
+    @CsvSource({
+            "1, 1",
+            "2, 2",
+            "4, 4"
+    })
+    void normal_numbers_should_be_printed_as_it_is(int number, String expectedRepresentation) {
+        assertThat(fizzBuzz(number)).isEqualTo(expectedRepresentation);
     }
+
+    @ParameterizedTest(name = "{0} should be Fizz")
+    @CsvSource({
+            "3",
+            "6",
+            "9"
+    })
+    void multiples_of_3_should_be_printed_as_Fizz(int number) {
+        assertThat(fizzBuzz(number)).isEqualTo("Fizz");
+    }
+
+    @ParameterizedTest(name = "{0} should be Buzz")
+    @CsvSource({
+            "5",
+            "10",
+            "25"
+    })
+    void multiples_of_5_should_be_printed_as_Buzz(int number) {
+        assertThat(fizzBuzz(number)).isEqualTo("Buzz");
+    }
+
+    @ParameterizedTest(name = "{0} should be FizzBuzz")
+    @CsvSource({
+            "15",
+            "45"
+    })
+    void multiples_of_3_and_5_should_be_printed_as_FizzBuzz(int number) {
+        assertThat(fizzBuzz(number)).isEqualTo("FizzBuzz");
+    }
+
 }
