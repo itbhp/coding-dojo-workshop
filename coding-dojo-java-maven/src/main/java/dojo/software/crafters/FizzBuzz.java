d61777176e81b320d6c2a3e27f08c8f18cbf18d8
Paolo Sciarra <itbhps@gmail.com>
2023-11-13 23:41:43 +0100

    live coding

--- coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
+++ coding-dojo-java-maven/src/main/java/dojo/software/crafters/FizzBuzz.java
@@ -1,8 +1,35 @@
 package dojo.software.crafters;
 
-public class FizzBuzz 
-{
+import java.util.List;
+
+public class FizzBuzz {
+    private static final NumberRule fizzRule = new NumberRule(3, "Fizz");
+    private static final NumberRule buzzRule = new NumberRule(5, "Buzz");
+    private static final List<Rule> rules = List.of(fizzRule, buzzRule);
+    private static final Rule identityRule = (n) -> "";
+    private static final Rule combinedRule = rules.stream().reduce(identityRule, FizzBuzz::combine);
+
     public static String fizzBuzz(int number) {
-        return "1";
+        String result = combinedRule.applyTo(number);
+        return result.isBlank() ? String.valueOf(number) : result;
+    }
+
+    private interface Rule {
+        String applyTo(int number);
+    }
+
+    private record NumberRule(int testNumber, String motto) implements Rule {
+        @Override
+        public String applyTo(int number) {
+            return number % testNumber == 0 ? motto : "";
+        }
+    }
+
+    private static Rule combine(Rule a, Rule b) {
+        return number -> {
+            var resultA = a.applyTo(number);
+            var resultB = b.applyTo(number);
+            return resultA + resultB;
+        };
     }
 }
--- coding-dojo-java-maven/src/test/java/dojo/software/crafters/FizzBuzzTest.java
+++ coding-dojo-java-maven/src/test/java/dojo/software/crafters/FizzBuzzTest.java
@@ -1,14 +1,27 @@
 package dojo.software.crafters;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static dojo.software.crafters.FizzBuzz.fizzBuzz;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
 
-    @Test
-    void one_should_print_one() {
-        assertThat(fizzBuzz(1)).isEqualTo("1");
+    @ParameterizedTest(name = "{0} should be {1}")
+    @CsvSource(value = {
+            "1, 1",
+            "2, 2",
+            "4, 4",
+            "3, Fizz",
+            "9, Fizz",
+            "12, Fizz",
+            "5, Buzz",
+            "25, Buzz",
+            "15, FizzBuzz",
+            "45, FizzBuzz"
+    })
+    void fizzBuzz_should_work(int number, String expected) {
+        assertThat(fizzBuzz(number)).isEqualTo(expected);
     }
 }
